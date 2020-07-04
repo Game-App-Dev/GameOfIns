c9b8cdc814ac46ddd976e794a96133bf25b1c440
Woongjin-J <woongjin1028@gmail.com>
2020-07-03 23:34:34 -0700

    starting page

---
+++ insurance.js
@@ -0,0 +1,58 @@
+(function() {
+  "use strict";
+
+  /**
+   * Add a function that will be called when the window is loaded.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * Calls the corresponding functions when buttons clicked.
+   */
+  function init() {
+    id("start").addEventListener("click", startGame);
+    // id("create-room").addEventListener("click", createRoom);
+    // id("join").addEventListener("click", joinGame);
+  }
+
+  /**
+   * Start button clicked and directs to menu page where displays rooms availability.
+   */
+  function startGame() {
+    id("start-view").classList.add("hidden");
+    id("menu-view").classList.remove("hidden");
+  }
+
+
+
+  /* ------------------------------ Helper Functions  ------------------------------ */
+  // Note: You may use these in your code, but do remember that your code should not have
+  // any functions defined that are unused.
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) { // less common, but you may find it helpful
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns all the element that matches the given css selector.
+   * @param  {[type]} selector - CSS query selector.
+   * @return {[type]} All DOM object matching the query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+})();
