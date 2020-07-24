76a28a4190449423778e9582b15faccb2e9df8fe
Woongjin-J <woongjin1028@gmail.com>
2020-07-23 22:52:18 -0700

    information page

--- insurance.js
+++ insurance.js
@@ -1,6 +1,8 @@
 (function() {
   "use strict";
 
+  let ended = false;
+
   /**
    * Add a function that will be called when the window is loaded.
    */
@@ -13,13 +15,23 @@
     id("start").addEventListener("click", startGame);
     id("menu").addEventListener("click", backToMenu);
     id("info").addEventListener("click", showInfo);
+    id("rules").addEventListener("click", showRule);
+    id("buy-yes").addEventListener("click", buyYes);
+    id("buy-no").addEventListener("click", noButton);
+    //id("save-yes").addEventListener("click", saveYes);
+    id("save-no").addEventListener("click", noButton)
   }
 
   /**
    * Start button clicked and directs to menu page where displays rooms availability.
    */
   function startGame() {
+    ended = false;
     id("start-view").classList.add("hidden");
+    id("insurance-type").classList.remove("hidden");
+    id("plan-selection").classList.add("hidden");
+    id("saving-plan").classList.add("hidden");
+    id("roll-page").classList.add("hidden");
     id("game-view").classList.remove("hidden");
   }
 
@@ -27,13 +39,64 @@
    * Menu button takes the user back to start page from game page.
    */
   function backToMenu() {
+    ended = true;
     id("start-view").classList.remove("hidden");
     id("game-view").classList.add("hidden");
   }
 
-  /***/
+  /**
+   * Display information page/window
+   */
   function showInfo() {
-    id("infoPage").classList.toggle("show");
+    id("myInfo").style.display = "block";
+    document.getElementsByClassName("close")[0].addEventListener("click", closePop);
+  }
+
+  function showRule() {
+    id("myRules").style.display = "block";
+    document.getElementsByClassName("close")[1].addEventListener("click", closePop);
+  }
+
+  // When the user clicks on <span> (x), close the popup
+  function closePop() {
+    id("myInfo").style.display = "none";
+    id("myRules").style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close myInfo page
+  window.addEventListener("click", function(event) {
+    if (event.target == id("myInfo")) {
+      id("myInfo").style.display = "none";
+    }
+  });
+
+  // When the user clicks anywhere outside of the modal, close myRules page
+  window.addEventListener("click", function(event) {
+    if (event.target == id("myRules")) {
+      id("myRules").style.display = "none";
+    }
+  });
+
+  function buyYes() {
+    id("insurance-type").classList.add("hidden");
+    id("plan-selection").classList.remove("hidden");
+    // id("plan-one").addEventListener("click", planOne);
+    // id("plan-two").addEventListener("click", planTwo);
+    // id("plan-three").addEventListener("click", planThree);
+    // id("plan-four").addEventListener("click", planFour);
+    id("plan-back").addEventListener("click", planBack);
+  }
+
+  function noButton() {
+    id("insurance-type").classList.add("hidden");
+    id("saving-plan").classList.add("hidden");
+    id("roll-page").classList.remove("hidden");
+    //id("roll-dice").addEventListener("click", rollDice);
+  }
+
+  function planBack() {
+    id("insurance-type").classList.remove("hidden");
+    id("plan-selection").classList.add("hidden");
   }
 
 
